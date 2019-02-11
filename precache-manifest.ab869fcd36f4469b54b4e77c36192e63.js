962291bc39c8dccad7c01edc32a8454ffb52118f
Sergio García <sergio.garcia@aiesec.net>
2019-02-11 13:29:41 -0600

    Updates

---
+++ precache-manifest.ab869fcd36f4469b54b4e77c36192e63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f1b96fefe5ffb99b4be",
+    "url": "/gid_assessment/static/js/runtime~main.4f1b96fe.js"
+  },
+  {
+    "revision": "636f82ef8b78c0dc5811",
+    "url": "/gid_assessment/static/js/main.636f82ef.chunk.js"
+  },
+  {
+    "revision": "558a0d81b2ec8e4beed3",
+    "url": "/gid_assessment/static/js/1.558a0d81.chunk.js"
+  },
+  {
+    "revision": "636f82ef8b78c0dc5811",
+    "url": "/gid_assessment/static/css/main.f3da6672.chunk.css"
+  },
+  {
+    "revision": "91758b1cb735d2025c68060d7ea0414d",
+    "url": "/gid_assessment/index.html"
+  }
+];
